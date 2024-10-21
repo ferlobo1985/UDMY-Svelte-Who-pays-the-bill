4c0c8a32a624e3d99ee45718e0106e1cf8d1a481
ferlobo1985 <fernando.lobo.prez@gmail.com>
2024-10-21 00:37:06 -0300

    02-The names component
    02-The names component

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,15 @@
 <script>
+    import { store } from "./store/app.store";
+    import Names from "./lib/components/names.svelte";
+    import Result from "./lib/components/result.svelte";
 
+    store.subscribe((obj)=>{
+      console.log(obj)
+    })
 </script>
 
-
-hello
+{#if $store.state}
+  <Names/>
+{:else}
+  <Result/>
+{/if}
---
+++ src/lib/components/names.svelte
@@ -0,0 +1,31 @@
+<script>
+
+
+</script>
+
+<div class="container">
+
+    <div class="logo">
+        <h1>Who pays the bill</h1>
+    </div>
+
+    <div class="input_container">
+        <input type="text"/>
+        <button>Add</button>
+    </div>
+
+    <!-- ERROR -->
+     
+    <div class="list_of_names">
+        <div>
+            Mike
+        </div>
+    </div>
+
+    <div
+        class="action_button"
+    >
+        Who's the Looser ?
+    </div>
+
+</div>
